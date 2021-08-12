144bae197039b574b24360a00a8724f1269f73fc
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-08-12 23:23:06 +0700

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,15 +3,12 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 4000 &>/dev/null &
-clear
 echo ==============================
 sudo apt-get update
-clear
 echo ==============================
 apt-get install ubuntu-desktop -y
 wget https://download.nomachine.com/download/7.6/Linux/nomachine_7.6.2_4_amd64.deb
 apt install ./nomachine_7.6.2_4_amd64.deb -y
-clear
 echo ==============================
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo ==============================
