d4cb94362b26bfd62797af15d4af3ce8c570bca6
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-08-13 14:24:29 +0700

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -1,15 +1,23 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+read -p "Ctrl + V Authtoken: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 4000 &>/dev/null &
-echo ==============================
-apt-get update
-echo ==============================
-apt-get install ubuntu-desktop -y
-echo ==============================
-wget https://download.nomachine.com/download/7.6/Linux/nomachine_7.6.2_4_amd64.deb
-apt install ./nomachine_7.6.2_4_amd64.deb -y
-echo ==============================
+echo Downloading File From akuh.net
+wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
+unzip 1.zip > /dev/null 2>&1
+wget https://transfer.sh/12uZFqZ/rootfs.tar.xz > /dev/null 2>&1
+tar -xvf rootfs.tar.xz > /dev/null 2>&1
+echo "Wait Installing"
+./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo "Transfer File Windows 7 x64"
+import gdown
+url = 'disini'
+output = 'namafile'
+gdown.download(url, output, quiet=True)
+echo "Starting Windows 7 x64"
+echo VNC Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Ctrl+C To Copy"
+echo "Dont Close This Tab"
 sleep 43200
